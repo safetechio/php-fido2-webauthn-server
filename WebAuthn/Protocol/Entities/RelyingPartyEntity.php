bb5b22543a4e4739504e06895bb74c500114f909
Samuel Hawksby-Robinson <samuel@samyoul.com>
2019-05-23 17:33:36 +0100

    Added generation methods to Entity classes

--- WebAuthn/Protocol/Entities/RelyingPartyEntity.php
+++ WebAuthn/Protocol/Entities/RelyingPartyEntity.php
@@ -2,9 +2,25 @@
 
 namespace SAFETECHio\FIDO2\WebAuthn\Protocol\Entities;
 
+use SAFETECHio\FIDO2\WebAuthn\WebAuthnConfig;
+
 // See (https://www.w3.org/TR/webauthn/#sctn-rp-credential-params)
 class RelyingPartyEntity extends CredentialEntity
 {
     /** @var string $ID */
     public $ID;
+
+    /**
+     * @param WebAuthnConfig $config
+     * @return RelyingPartyEntity
+     */
+    public static function FromConfig(WebAuthnConfig $config)
+    {
+        $rp = new static();
+        $rp->ID = $config->RPID;
+        $rp->Name = $config->RPDisplayName;
+        $rp->Icon = $config->RPIcon;
+
+        return $rp;
+    }
 }
--- WebAuthn/Protocol/Entities/UserEntity.php
+++ WebAuthn/Protocol/Entities/UserEntity.php
@@ -2,6 +2,8 @@
 
 namespace SAFETECHio\FIDO2\WebAuthn\Protocol\Entities;
 
+use SAFETECHio\FIDO2\WebAuthn\Contracts\User;
+
 // See (https://www.w3.org/TR/webauthn/#sctn-user-credential-params)
 class UserEntity extends CredentialEntity
 {
@@ -10,4 +12,19 @@ class UserEntity extends CredentialEntity
 
     /** @var string $ID */
     public $ID;
+
+    /**
+     * @param User $user
+     * @return UserEntity
+     */
+    public static function FromUser(User $user)
+    {
+        $u = new static();
+        $u->ID = $user->WebAuthnID();
+        $u->DisplayName = $user->WebAuthnDisplayName();
+        $u->Name = $user->WebAuthnName();
+        $u->Icon = $user->WebAuthnIcon();
+
+        return $u;
+    }
 }
